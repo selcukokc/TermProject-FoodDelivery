b657d6c69fb7d099b420c6c23bb4e49dba7879cf
selcukokc <selcukokuyucu@gmail.com>
2021-05-22 12:53:10 +0300

    added restaurant panel fragments

---
+++ app/src/main/java/com/selcukokc/fooddelivery/view/RestaurantPanelFragment.kt
@@ -0,0 +1,28 @@
+package com.selcukokc.fooddelivery.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.selcukokc.fooddelivery.R
+
+
+class RestaurantPanelFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_restaurant_panel, container, false)
+    }
+
+
+}
