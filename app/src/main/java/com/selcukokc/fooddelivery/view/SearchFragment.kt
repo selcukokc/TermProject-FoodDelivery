1790da725df52f81c44a7aa94b00bd257de3ed41
selcukokc <selcukokuyucu@gmail.com>
2021-05-22 18:29:37 +0300

    implemented searchview

--- app/src/main/java/com/selcukokc/fooddelivery/view/SearchFragment.kt
+++ app/src/main/java/com/selcukokc/fooddelivery/view/SearchFragment.kt
@@ -1,19 +1,19 @@
 package com.selcukokc.fooddelivery.view
 
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.ListFragment
 import com.selcukokc.fooddelivery.R
 
-class SearchFragment : BaseFragment() {
+class SearchFragment: BaseFragment(), SearchView.OnQueryTextListener {
     override var bottomNavigationViewVisibility = View.VISIBLE
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -22,6 +22,23 @@ class SearchFragment : BaseFragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+        val item = menu.findItem(R.id.search_item)
+        val searchView = item.actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+       return true
+    }
+
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
     }
 
 
